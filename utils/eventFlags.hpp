163300d3d24c3833c420043a40b68bd102730c65
Nymic <named.blade@gmail.com>
2024-08-01 02:11:14 +0100

    Even flag handling improvements

--- utils/eventFlags.hpp
+++ utils/eventFlags.hpp
@@ -4,17 +4,21 @@
 
 using namespace ModUtils;
 
+struct EventFlagMan{
+	void* ptr;
+};
+
 std::string getEventFlagValueAob = "44 8b 41 1c 44 8b da 33 d2 41 8b c3 41 f7 f0 4c 8b d1 45 33 c9 44 0f af c0 45 2b d8 4c 8b 41 38 49 8b d0 49 8b 48 08 44 38 49 19";
-using eventFlagGetterType = bool(*)(uintptr_t, unsigned int);
+using eventFlagGetterType = bool(*)(EventFlagMan, unsigned int);
 eventFlagGetterType eventFlagGetter;
 std::string setEventFlagValueAob = "48 89 5c 24 08 44 8b 49 1c 44 8b d2 33 d2 41 8b c2 41 f7 f1 41 8b d8 4c 8b d9 4c 8b 41 38 44 0f af c8 49 8b c8 49 8b 50 08";
-using eventFlagSetterType = bool(*)(uintptr_t, unsigned int, bool);
+using eventFlagSetterType = bool(*)(EventFlagMan, unsigned int, bool);
 eventFlagSetterType eventFlagSetter;
 
 std::string eventFlagManAob = "48 8d 0d 6d c9 44 02 e8 28 de 93 01 48 8b 1d 29 d3 7e 03 48 8b d6 48 8d 4c 24 20 e8 24 ce 05 00";
 int eventFlagManAobOffset = 15;
 int eventFlagManInstructionSize = eventFlagManAobOffset+4;
-uintptr_t eventFlagMan;
+EventFlagMan* eventFlagMan;
 
 bool isEventFlagInit = false;
 
@@ -39,7 +43,7 @@ void initFlagHandlers(){
 			
 			if (address != 0)
 			{
-				eventFlagMan = address + *(std::uint32_t*)(address+eventFlagManAobOffset) + eventFlagManInstructionSize;
+				eventFlagMan = (EventFlagMan*)(address + *(std::uint32_t*)(address+eventFlagManAobOffset) + eventFlagManInstructionSize);
 			}
 		}
 		
@@ -53,7 +57,7 @@ bool getEventFlagState (unsigned int flag)
 	}
 	
 	if (eventFlagMan != 0 && eventFlagGetter != 0){
-		return eventFlagGetter(*(uintptr_t*)eventFlagMan,flag);
+		return eventFlagGetter(*eventFlagMan,flag);
 	} else {
 		return false;
 	}
@@ -74,7 +78,7 @@ void setEventFlagState (unsigned int flag, bool state){
 	}
 	
 	if (eventFlagMan != 0 && eventFlagGetter != 0){
-		eventFlagSetter(*(uintptr_t*)eventFlagMan,flag,state);
+		eventFlagSetter(*eventFlagMan,flag,state);
 	}
 }
 
