cd69a5a487217f23ba2a3c5c4ed4ea17c4197895
Nymic <named.blade@gmail.com>
2024-08-04 02:16:51 +0100

    DS2 inspired health scaling

--- mods/attributeMod/attributeGetters.cpp
+++ mods/attributeMod/attributeGetters.cpp
@@ -18,5 +18,7 @@ float getCalcCorrectGraphHook(float value, int row){
 
 int getMaxHPByStatsHook(uintptr_t playerData){
     AttributeData attributeData = *(AttributeData*)(playerData+attributeDataOffset);
-	return getCalcCorrectGraph(attributeData.vigor + ((getPlayerLevel(attributeData) - attributeData.vigor)/7)*0.35,100);
+    float addHp = (getPlayerLevel(attributeData) - attributeData.vigor - 70) * (600.0 / (89.0*7.0));
+    float finalHp =  getCalcCorrectGraph(attributeData.vigor,100);
+	return finalHp + addHp;
 }
