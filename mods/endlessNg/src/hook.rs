40c55a3f8a68f97dc99e93440636422f02da3f89
Nymic <named.blade@gmail.com>
2026-02-22 19:56:53 +0000

    msg hook

--- mods/endlessNg/src/hook.rs
+++ mods/endlessNg/src/hook.rs
@@ -1,14 +1,17 @@
+use closure_ffi::traits::FnPtr;
+use windows::core::w;
+use windows_sys::core::PCWSTR;
 use std::mem;
 use std::sync::OnceLock;
 use eldenring::{
     param::EQUIP_PARAM_GOODS_ST
 };
-use winhook::{HookHandle, HookInstaller};
+use winhook::{CConv, HookHandle, HookInstaller};
 use pelite::pattern::parse;
 use crate::patch::*;
 
-const GET_GOODS_AOB: &str = "8b 12 48 8d 4d c0 83 cf ff 89 75 c4 89 7d c0 48 89 75 c8 e8 ? ? ? ? 48 8b 45 c8 48 85 c0 74 03 8b 78 74 8b c7 eb 32";
-const GET_GOODS_OFFSET: usize = 20;
+pub const GET_GOODS_AOB: &str = "8b 12 48 8d 4d c0 83 cf ff 89 75 c4 89 7d c0 48 89 75 c8 e8 ? ? ? ? 48 8b 45 c8 48 85 c0 74 03 8b 78 74 8b c7 eb 32";
+pub const GET_GOODS_OFFSET: usize = 20;
 
 #[repr(C, packed)]
 pub struct GetGoodsResult {
@@ -25,43 +28,44 @@ pub type GetGoodsType = unsafe extern "C" fn(
 pub static GET_GOODS_ORIGINAL_HOLDER: OnceLock<GetGoodsType> = OnceLock::new();
 static INTENSITY_LEVEL_GOODS: OnceLock<EQUIP_PARAM_GOODS_ST> = OnceLock::new();
 
-const GET_MESSAGE_AOB: &str = "75 24 44 8b cb 33 d2 41 b8 9c 01 00 00 48 8b cf e8 ? ? ? ? 48 85 c0 48 8d 0d ? ? ? ? 48 0f 45 c8 48 8b c1 48 8b 5c 24 30";
-const GET_MESSAGE_OFFSET: usize = 17;
+pub const GET_MESSAGE_AOB: &str = "75 24 44 8b cb 33 d2 41 b8 9c 01 00 00 48 8b cf e8 ? ? ? ? 48 85 c0 48 8d 0d ? ? ? ? 48 0f 45 c8 48 8b c1 48 8b 5c 24 30";
+pub const GET_MESSAGE_OFFSET: usize = 17;
 
 pub type GetMessageType = unsafe extern "C" fn(
     message_repository: usize,
     _1: u32,
     msg_bnd: u32,
     msg_id: u32,
-);
+) -> PCWSTR;
 
 pub static GET_MESSAGE_ORIGINAL_HOLDER: OnceLock<GetMessageType> = OnceLock::new();
 
-pub fn init_hooks() -> Option<HookHandle> {
-    let Some(address) = aob_scan(&parse(GET_GOODS_AOB).unwrap()) else {return None};
+pub fn get_addr_from_call (address: usize) -> usize {
     unsafe {
-        let address = address.wrapping_add(GET_GOODS_OFFSET);
         let address = (address as isize)
             .wrapping_add(mem::size_of::<i32>() as isize)
             .wrapping_add(std::ptr::read_unaligned(address as *const i32) as isize)
-            as *mut GetGoodsType;
-        let func: GetGoodsType = std::mem::transmute(address);
-        GET_GOODS_ORIGINAL_HOLDER.set(func).ok();
+            as usize;
+        return address;
     }
+}
 
-    let Some(address) = aob_scan(&parse(GET_MESSAGE_AOB).unwrap()) else {return None};
+pub fn make_installer_from_call_aob<T: FnPtr + CConv + 'static> (aob: &str, offset: usize, holder: &OnceLock::<T>) -> Option::<HookInstaller::<T>> {
+    let Some(address) = aob_scan(&parse(aob).unwrap()) else {return None};
     unsafe {
-        let address = address.wrapping_add(GET_MESSAGE_OFFSET);
-        let address = (address as isize)
-            .wrapping_add(mem::size_of::<i32>() as isize)
-            .wrapping_add(std::ptr::read_unaligned(address as *const i32) as isize)
-            as *mut GetMessageType;
-        let func: GetMessageType = std::mem::transmute(address);
-        GET_MESSAGE_ORIGINAL_HOLDER.set(func).ok();
+        let address = address.wrapping_add(offset);
+        let address = get_addr_from_call(address as usize) as *mut T;
+        let func: T = std::mem::transmute_copy::<*mut T, T>(&address);
+        holder.set(func).ok();
     }
+    let Some(func) = holder.get() else {return None};
+    let hook = HookInstaller::<T>::for_function(*func);
+    return Some(hook);
+}
 
-    let Some(get_goods_original) = GET_GOODS_ORIGINAL_HOLDER.get() else {return None};
-    let hook_handle_goods = HookInstaller::<GetGoodsType>::for_function(*get_goods_original)
+pub fn init_hooks() -> Vec<HookHandle> {
+    let installer_get_goods = make_installer_from_call_aob::<GetGoodsType>(GET_GOODS_AOB, GET_GOODS_OFFSET, &GET_GOODS_ORIGINAL_HOLDER);
+    let hook_handle_goods = installer_get_goods.unwrap()
     .install_mut({
         move |original| move |result, id| {
             if id == 67350{
@@ -75,15 +79,28 @@ pub fn init_hooks() -> Option<HookHandle> {
                     (*result).id = 67350;
                     (*result).row = INTENSITY_LEVEL_GOODS.get().unwrap() as *const _ as *mut _;
                 }
-            }
-            else {
+            } else {
                 unsafe { original(result, id) };
             }
         }
-    })
-    .unwrap();
+    }).unwrap();
 
-    unsafe {hook_handle_goods.enable(true)};
+    let installer_get_message = make_installer_from_call_aob::<GetMessageType>(GET_MESSAGE_AOB, GET_MESSAGE_OFFSET, &GET_MESSAGE_ORIGINAL_HOLDER);
+    let hook_handle_message = installer_get_message.unwrap()
+    .install_mut({
+        move |original| move |message_repository, _1, msg_bnd, msg_id| {
+            if msg_id == 67350 && msg_bnd == 10 {
+                return w!("Modify Intensity By:").as_ptr();
+            } else {
+                return unsafe { original(message_repository, _1, msg_bnd, msg_id) };
+            }
+        }
+    }).unwrap();
 
-    return Some(hook_handle_goods);
+    unsafe {
+        hook_handle_goods.enable(true);
+        hook_handle_message.enable(true);
+    };
+
+    return vec![hook_handle_goods, hook_handle_message];
 }
